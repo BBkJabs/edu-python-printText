9cbfaee948fc1d9dbd9bfc0f65972105770566ea
M. Jabs <80675651+BBkJabs@users.noreply.github.com>
2025-03-11 09:16:48 +0100

    Update printText.py
    Aus der Funktion main() gemacht

--- printText.py
+++ printText.py
@@ -1,2 +1,6 @@
-def hello_world():
+def main():
     return "Die Tasten klappern, Ideen sprühen, Probleme lösen, Fehler mühen. Doch am Ende, wenn es läuft, Ist es das, was uns erfreut."
+
+
+if __name__ == "__main__":
+    main()
