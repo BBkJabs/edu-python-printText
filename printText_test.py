dda0c995121234c7b6aba8b05d7319697af4e674
M. Jabs <80675651+BBkJabs@users.noreply.github.com>
2025-03-11 09:16:17 +0100

    Update printText_test.py
    Fehler: printText.main() -> hello.main()

--- printText_test.py
+++ printText_test.py
@@ -2,7 +2,7 @@ import hello;
 
 def test_hello():
     expected_output = "Die Tasten klappern, Ideen sprühen,\nProbleme lösen, Fehler mühen.\nDoch am Ende, wenn es läuft,\nIst es das, was uns erfreut."
-    actual_output = hello.hello_world()
+    actual_output = printText.main()
     
     # Normalize line endings and remove trailing spaces at line endings
     normalized_actual = actual_output.replace('\r\n', '\n').strip()
